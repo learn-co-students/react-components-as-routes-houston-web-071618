c475e6e592a045a6dcfd6fd03d5d2a2e6cfe85d6
github username <github email address>
2018-09-27 10:54:25 -0500

    Done.

--- src/Login.js
+++ src/Login.js
@@ -2,19 +2,18 @@ import React, { Component } from "react";
 class Login extends React.Component {
   render() {
     return (
-      <div>
-        <form>
-          <div>
-            <input type="text" name="username" placeholder="Username" />
-            <label htmlFor="username">Username</label>
-          </div>
-          <div>
-            <input type="password" name="password" placeholder="Password" />
-            <label htmlFor="password">Password</label>
-          </div>
-          <input type="submit" value="Login" />
-        </form>
-      </div>
+      <form>
+        <h1>Login</h1>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
     );
   }
 }
